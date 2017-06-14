695cb22d26058b49e6eb893874570c2b16ab6340
eyaleatafekaacil <eyale@afeka.ac.il>
2017-06-14 04:26:01 +0300

    initializing maven project

---
+++ README.md
@@ -0,0 +1,5 @@
+# maven-demo
+This project demonstrates a publicly available maven project for cloning
+
+
+
---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.err.println("Hello World!");
+	}
+
+}
